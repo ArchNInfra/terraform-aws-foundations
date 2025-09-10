7c87e7126a3fa0c26b47294f7a2cbc404259fdef
ArchNInfra <221547425+ArchNInfra@users.noreply.github.com>
2025-09-10 15:53:59 +0800

    feat(T03): EC2 + dynamic AMI + outputs (apply/destroy verified)

--- archive/T03/ec2.tf
+++ archive/T03/ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "web" {
-  ami                    = data.aws_ami.al2023.id
-  instance_type          = "t3.micro"
+  ami           = data.aws_ami.al2023.id
+  instance_type = "t3.micro"
 
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
 
-  iam_instance_profile   = "EC2SSMRole"
+  iam_instance_profile = "EC2SSMRole"
 
   tags = {
     Name = "t3-web-ssm"
