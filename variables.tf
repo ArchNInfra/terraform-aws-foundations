f1d6c95cfdb96f5ee854cf391c97f44cd1acce16
ArchNInfra <221547425+ArchNInfra@users.noreply.github.com>
2025-09-10 14:30:30 +0800

    chore(T03): define subnet_id & security_group_id variables

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "subnet_id" {
+  description = "Public subnet ID (e.g. subnet-xxxxxxxx)"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "Security Group ID (e.g. sg-xxxxxxxx)"
+  type        = string
+}
